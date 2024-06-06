ce53f4672b64b65c72717cf21e9a766ecaf04e3c
zont <jay0909258118@gmail.com>
2024-06-07 06:45:30 +0800

    - add front-end and backend - add rest button - add related styles

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from fastapi import FastAPI , Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from typing import Optional
 app = FastAPI()
 app.mount("/static",StaticFiles(directory="static"), name = "static")
 
 templates = Jinja2Templates(directory= "templates")
 
 @app.get("/")
-def get(request: Request):
+def get(request: Request , board :Optional[str] = "000000000"):
+    
+    count_dict = {'1':0 , "2":0}
+    for c in board:
+        if c == '1':
+            count_dict['1'] += 1
+        if c == '2':
+            count_dict['2'] += 1
+    
+    if count_dict['1'] == count_dict['2'] :
+        turn = '1'
+    if count_dict['1'] - count_dict['2'] == 1:
+        turn = '2'
+    else:
+        raise ValueError("board inbalid")
+    
+    
     return templates.TemplateResponse(
         "tic_tac_toe.html",
-        {"request": request}
+        {
+            "request": request,
+            "board" : board,
+            "turn" : turn
+        }
     )
